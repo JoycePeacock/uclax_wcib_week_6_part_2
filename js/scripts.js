5572847c972ddcf0db555398b857d3bcf942df2b
Joyce <jpeacock@cypresscollege.edu>
2020-11-11 21:45:08 -0800

    java fixes

--- js/scripts.js
+++ js/scripts.js
@@ -10,24 +10,19 @@ btnPrimary.onclick = function() {
   const rndCol = 'rgb(' + random(255) + ',' + random(255) + ',' + random(255) + ')';
   document.getElementById('container').style.backgroundColor = rndCol;
 }
-/*
+
 //beer quotes
-const beerQuote = document.querySelector('.beerquote');
-const story = document.querySelector('.story');
+var slideIndex = 0;
+showSlides();
 
-function randomValueFromArray(array){
-  const random = Math.floor(Math.random()*array.length);
-  return array[random];
-}
-
-let storyText = ":insertx:";
-
-let insertX = ['\"What care I how time advances? I am drinking ale today.\"', '\"He was a wise man who invented beer.\"' , '\"Beer, it’s the best damn drink in the world.\"', '\"A man who lies about beer makes enemies.\"'
-
-(".beerquote").addEventListener('click', function result() {
-  let newStory = storyText;
-  let xItem = randomValueFromArray (insertX);
-  newStory = newStory.replace(':insertx:',xItem);
-})
-  story.textContent = newStory;
-  story.style.visibility = 'visible';*/
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 4000);
+} // Change image every 4 seconds/*
